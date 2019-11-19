629e1059eb7114f411182dcc0fe7b786694a44d5
E.KyIibin <evgenii.kylibin@gmail.com>
2019-11-19 17:01:30 +0300

    Installed bs4

--- scrapper/models.py
+++ scrapper/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
 
 # Create your models here.
 
-class Scrapper(models.Model):
-    name = models.CharField(max_length=140)
-    slug = models.SlugField()
+# def scrapper_bot(url):
+#     try:
+#         html = urlopen(url)
+#     except HTTPError as e:
+#         return None
+#     try:
+#         bs
+
     
