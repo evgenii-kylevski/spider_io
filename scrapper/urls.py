7f4ae23279eabdfc7de92b651a346527fc883e9d
E.KyIibin <evgenii.kylibin@gmail.com>
2019-12-03 22:48:43 +0300

    Pages created.

--- scrapper/urls.py
+++ scrapper/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from scrapper.views import *
 
 urlpatterns = [
-    path('', scrapper_home),
-    path('scrapper', scrapper_app),
-    path('reports', reports_list),
-    path('scrap-set', scrapper_set),
+    path('', scrapper_home, name="home"),
+    path('scrapper/', scrapper_app, name="scrapper"),
+    path('reports/', reports_list, name="reports"),
+    path('scrap-set/', scrapper_set, name="scrap-set"),
 ]
