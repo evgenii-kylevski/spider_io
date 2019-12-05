27d1ed8687921d5fa8cc3fbe7e0e58c0fdffaa5b
E.KyIibin <evgenii.kylibin@gmail.com>
2019-12-05 11:50:00 +0300

    Ready for development

--- scrapper/models.py
+++ scrapper/models.py
@@ -4,13 +4,3 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 # Create your models here.
-
-# def scrapper_bot(url):
-#     try:
-#         html = urlopen(url)
-#     except HTTPError as e:
-#         return None
-#     try:
-#         bs
-
-    
